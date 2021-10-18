fc16040dcb2897fd6bca001e585321c3a41969ff
Marichka2003 <91660359+Marichka2003@users.noreply.github.com>
2021-10-18 21:56:15 +0300

    Add files via upload

---
+++ Homework_4/Homework_4/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Homework_4
+{
+    public class Person
+    {
+        private string name { get; set; }
+        private DateTime birthYear { get; set; }
+        public int age;
+        public Person() { }
+
+        public Person(string new_name, DateTime new_birthYear)
+        {
+            name = new_name;
+            birthYear = new_birthYear;
+        }
+
+        public void Age()
+        {
+            age = DateTime.Now.Year - birthYear.Year;
+            Console.WriteLine($"{name} is {age}");
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter name of person");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter birth of person");
+            birthYear = Convert.ToDateTime(Console.ReadLine());
+        }
+        public void ChangeName()
+        {
+            Console.WriteLine("Change the name");
+            name = "Very Young";
+        }
+        public void Output()
+        {
+            name.ToString();
+        }
+        public static bool operator ==(Person first, Person second)
+        {
+            return first.name == second.name;
+        }
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first.name == second.name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[6];
+            for (int i = 0; i < 6; i++)
+            {
+                person[i] = new Person();
+                person[i].Input();
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                person[i].Output();
+                person[i].Age();
+                if (person[i].age < 16)
+                {
+                    person[i].ChangeName();
+                }
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                person[i].Output();
+            }
+            for (int i = 0; i < 6; i++)
+            {
+
+                if (i == 4)
+                {
+                    if (person[i] == person[i + 1])
+                    {
+                        person[i].Output();
+                        person[i].Age();
+                    }
+                    break;
+                }
+                if (person[i] == person[i + 1])
+                {
+                    person[i].Output();
+                    person[i].Age();
+                }
+            }
+        }
+    }
+}
---
+++ Task_4/Task_4/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Task_4
+{
+    public class Car
+    {
+        private string name;
+        public string color { get; set; }
+        private double price;
+        private const string CompanyName = "BMW";
+       
+
+        public Car() { }
+
+
+        public Car(string new_name,string new_color, double new_price)
+        {
+            name = new_name;
+            color = new_color;
+            price = new_price;
+        }
+
+        public void Input()
+        {
+            Console.Write("Name: ");
+            name =  Console.ReadLine();
+            Console.Write("Color: ");
+            color = Console.ReadLine();
+            Console.Write("Price: ");
+            price = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"The mark of car is {CompanyName}, model is {name}, color {color} and this satisfaction cost {price}");
+        }
+
+        public void ChangePrice(double x)
+        {
+            price -= (price / 100)*x;
+            Console.Write($"{name} , {price}");
+        }
+        public static bool operator ==(Car first, Car second)
+        {
+            return first.name == second.name  || first.price == second.price;
+        }
+        public static bool operator !=(Car first, Car second)
+        {
+            return !(first.name == second.name && first.price == second.price);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car first = new Car();
+            Car second = new Car();
+            Car third = new Car();
+            first.Input();
+            first.Print();
+            second.Input();
+            second.Print();
+            third.Input();
+            third.Print();
+            first.ChangePrice(10);
+
+            if(first.color == "white")
+            {
+                Console.WriteLine("Enter the color: ");
+                first.color = Console.ReadLine();
+                Console.WriteLine($"Change color the first car to {first.color}");
+            }
+            if (second.color == "white")
+            {
+                Console.WriteLine("Enter the color: ");
+                second.color = Console.ReadLine();
+                Console.WriteLine($"Change color the first car to {second.color}");
+            }
+            if (third.color == "white")
+            {
+                Console.WriteLine("Enter the color: ");
+                third.color = Console.ReadLine();
+                Console.WriteLine($"Change color the first car to {third.color}");
+            }
+            if (first == second )
+            {
+                Console.WriteLine("The same names ");
+            }
+            else
+            {
+                Console.WriteLine("Not the same names ");
+            }
+                
+        }
+    }
+}
