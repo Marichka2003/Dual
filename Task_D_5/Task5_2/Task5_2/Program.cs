112b61d39536591f079896f2994cd9fdc7200435
Maruska <91660359+Marichka2003@users.noreply.github.com>
2021-10-26 00:13:53 +0300

    Add files via upload

---
+++ Task_D_5/Task5_2/Task5_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Task5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myColl = new ArrayList();
+            for(int i = 0, b = 10; i< 10; i++)
+            {
+                Console.Write($"Enter {b} number :");
+                myColl.Add(Convert.ToInt32(Console.ReadLine()));
+                b--;
+            }
+            Console.WriteLine();
+            Console.Write("Index of number \"-10\" : ");
+            for (int i = 0 , n = 10; i < n ; i++)
+            {
+                if ((int)myColl[i] == -10)
+                {
+                    Console.Write($"{i} ");
+                }                
+            }
+            Console.WriteLine();
+            Console.Write("Remote number more than 20: ");
+            for (int i = 0,  n = 10; i < n; i++)
+            {
+                if ((int)myColl[i] > 20)
+                {
+                    myColl.Remove(myColl[i]);
+                    i--;
+                    n--;
+                }
+                else
+                {
+                    Console.Write($"{myColl[i]} ");
+                }
+            }
+           
+            Console.WriteLine();
+            myColl.Insert(2,1);
+            myColl.Insert(8,-3);
+            myColl.Insert(5,-4);
+            Console.Write("Insert list: ");
+            foreach (object i in myColl)
+            {
+                Console.Write($" {i} ");
+            }
+            Console.WriteLine();
+            myColl.Sort();
+            Console.Write("Sort list: ");
+            foreach (object i in myColl)
+            {
+                Console.Write($" {i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Task_D_5/Task_5/Task_5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task_5
+{
+    public interface IFlyable
+    {
+        void Fly();
+    }
+    public class Bird : IFlyable
+    {
+        public string name { get; set; }
+        public string canFly { get; set; }
+        public void Fly()
+        {
+            Console.WriteLine($"name : {name} and this bird {canFly}");
+        }
+        
+    }
+    public class Plane : IFlyable
+    {
+        public string mark { get; set; }
+        public int hightFly { get; set; }
+        public void Fly()
+        {
+            Console.WriteLine($"mark : {mark} and {hightFly} km");
+        }
+    }
+    public class Space : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IFlyable> fly = new List<IFlyable>
+            {
+                new Bird{name = "Swallow",canFly = "can fly"},
+                new Bird{name = "Owl",canFly = "can fly"},
+                new Bird{name = "Hummingbird",canFly = "can fly"},
+                new Bird{name = "Flamingo",canFly = "can't fly"},
+                new Space{},
+                new Plane{mark = "Aviatech",hightFly= 35},
+                new Plane{mark = "Neman",hightFly=27},
+                new Plane{mark = "Sigma-5",hightFly=31},
+                new Plane{mark = "Yakutia",hightFly=24},
+            };
+           
+            foreach (IFlyable i in fly )
+            {
+                i.Fly();
+            }
+        }
+    }
+}
